b1a9578be0bffaa064a7a046445b8a32a41981a9
sitalpaudel <sitalpaudel@vt.edu>
2026-02-01 19:56:37 -0500

    Update MemManager.java
    constructor and insert

--- src/MemManager.java
+++ src/MemManager.java
@@ -15,17 +15,47 @@ public class MemManager {
      * @param startSize
      *            Initial size of the memory pool
      */
-    int[] memory;
+    private int k;
+    private byte[] memory;
+    private int[][] offsets;
+    private int[] count;
+    
     public MemManager(int startSize) {
-        memory = new int[startSize];
+        memory = new byte[startSize];
+        int blockSize = 1;
+        k = 0;
+        while (blockSize < startSize) {
+            blockSize *= 2;
+            k+=1;
+        }
+        offsets = new int[k+1][startSize];
+        count = new int[k+1];
+        offsets[k][0] = 0;
+        count[k] = 1;
     }
     
-    public int[] buddyMethod(int size) {
-        int blockSize = 2;
+    public Handle insert(String record) {
+        
+        byte[] storage = record.getBytes();
+        int size = storage.length;
+        int index = buddyMethod(size);
+        
+        
+        
+    }
+    
+    public int buddyMethod(int size) {
+        int blockSize = 1;
+        int k = 0;
         while (blockSize < size) {
             blockSize *= 2;
+            k+=1;
         }
-        memory[blockSize]
+        return k;
     }
+    
+    
+    //insert method
+    //find method if argument is handle, return string
     
 }
